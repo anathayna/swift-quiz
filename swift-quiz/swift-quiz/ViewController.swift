4489d56d1cb899be1ca9a4080006a9f71dcb6c91
ANA THAYNA CONCEICAO FRANCA <ana.tcfranca@cas411588maca06.local>
2019-11-05 19:02:09 -0300

    organizando os view controllers, necessita ajustes

--- swift-quiz/swift-quiz/ViewController.swift
+++ swift-quiz/swift-quiz/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var onGoMainButton: UIView!
+    
+    @IBAction func noGoMainButton(_ sender: Any) {
+        performSegue(withIdentifier: "Main", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
