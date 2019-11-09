e177401cc4feaa732532bc8065156a631fe93274
ANA THAYNA CONCEICAO FRANCA <ana.tcfranca@cas450193maca05.local>
2019-11-09 12:44:06 -0300

    adicionando algumas mudanças e começando a organizar as structs

--- swift-quiz/swift-quiz/QuizViewController.swift
+++ swift-quiz/swift-quiz/QuizViewController.swift
@@ -8,14 +8,57 @@
 
 import UIKit
 
+
+//MARK: - Structs
+struct Question {
+    var Question : String!
+    var Answers : [String]!
+    var Answer : Int!
+    //var Image : String!
+}
+
+
 class QuizViewController: UIViewController {
 
+    
+    //MARK: - Outlets
+    @IBOutlet var QLabel: UILabel!      //Pergunta
+    @IBOutlet var Buttons: [UIButton]!    //Opções
+    
+    
+    //MARK: - Variables
+    var Questions = [Question]()
+    var QNumber = Int()
+    var AnswerNumber = Int()
+    
+    
+    //MARK: - Lyfe Cicle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        Questions = [Question(Question: "o que é o que é", Answers: ["ué","coé","soé","poisé"], Answer: 3)]
     }
     
+    
+    //MARK: - Functions
+    func PickQuestion(){
+        if Questions.count > 0{
+            QNumber = 0
+            QLabel.text = Questions[QNumber].Question
+            
+            for i in 0..<Buttons.count{
+                Buttons[i].setTitle(Questions[QNumber].Answers[i], for: UIControl.State.normal)
+            }
+            
+            Questions.remove(at: QNumber)
+            
+        }
+        else{
+            NSLog("Fechou!")
+        }
+    }
 
     /*
     // MARK: - Navigation
