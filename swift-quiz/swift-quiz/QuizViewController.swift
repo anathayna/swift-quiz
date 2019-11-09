f053c77ff34dae89b0749e304656a3b27846cf25
anathayna <anathaynafranca@gmail.com>
2019-11-09 17:40:13 -0300

    adicionando dicionário necessita ajustes

--- swift-quiz/swift-quiz/QuizViewController.swift
+++ swift-quiz/swift-quiz/QuizViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 
-//MARK: - Structs
-struct Question {
-    var Question : String!
-    var Answers : [String]!
-    var Answer : Int!
-    //var Image : String!
-}
+////MARK: - Structs
+//struct Question {
+//    var Question : String!
+//    var Answers : [String]!
+//    var Answer : Int!
+//    //var Image : String!
+//}
 
 
 class QuizViewController: UIViewController {
@@ -24,42 +24,94 @@ class QuizViewController: UIViewController {
     //MARK: - Outlets
     @IBOutlet var QLabel: UILabel!      //Pergunta
     @IBOutlet var Buttons: [UIButton]!    //Opções
+    @IBOutlet var PView: UIProgressView! //Barra de progresso
     
     
     //MARK: - Variables
-    var Questions = [Question]()
-    var QNumber = Int()
+    var Questins: [String: String] = [:]
+    var QNumber = 0
     var AnswerNumber = Int()
+    var QProgress = Float()
+    let TotalQuestions = 5.0
     
     
     //MARK: - Lyfe Cicle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
-        Questions = [Question(Question: "o que é o que é", Answers: ["ué","coé","soé","poisé"], Answer: 3)]
+        // Pegar questões e imagens da struct de perguntas, no QuizViewController.swift
+        //Questions = [Q1: ["O que é o que é": "Ué", "Coé", "Soé", "Poisé" ], Q2: [], Q3: [], Q4: [], Q5: []]
+            //[Question(Question: "o que é o que é", Answers: ["ué","coé","soé","pois é"], Answer: 0)]
+        PickQuestion(Question: 0)
+        
     }
     
     
     //MARK: - Functions
-    func PickQuestion(){
-        if Questions.count > 0{
-            QNumber = 0
-            QLabel.text = Questions[QNumber].Question
-            
-            for i in 0..<Buttons.count{
-                Buttons[i].setTitle(Questions[QNumber].Answers[i], for: UIControl.State.normal)
-            }
-            
-            Questions.remove(at: QNumber)
-            
+    func PickQuestion(Question: Int){
+//        if Questions.count > 0{
+//            //QNumber = 0
+//            print(Questions[QNumber].Question, QNumber)
+//            QLabel.text = Questions[QNumber].Question
+//            AnswerNumber = Questions[QNumber].Answer
+//
+//            for i in 0..<Buttons.count{
+//                Buttons[i].setTitle(Questions[QNumber].Answers[i], for: UIControl.State.normal)
+//            }
+//
+//            //Remove a questão respondida da lista
+//            //Questions.remove(at: QNumber)
+//            
+//            QNumber += 1
+//            QProgress = Float(QNumber)/5
+//            PView.setProgress(QProgress, animated: true)
+//
+//            print("rodou")
+//
+//            PickQuestion(Question: QNumber)
+//        }
+//        else{
+//            NSLog("Fechou!")
+//        }
+    }
+    
+    //MARK: - Actions
+    @IBAction func btn1(_ sender: Any) {
+        if AnswerNumber == 0{
+            PickQuestion(Question: 0)
         }
         else{
-            NSLog("Fechou!")
+            NSLog("ERRROUUU!")
         }
     }
-
+    
+    @IBAction func btn2(_ sender: Any) {
+        if AnswerNumber == 1{
+            PickQuestion(Question: 0)
+        }
+        else{
+            NSLog("ERRROUUU!")
+        }
+    }
+    
+    @IBAction func btn3(_ sender: Any) {
+        if AnswerNumber == 2{
+            PickQuestion(Question: 0)
+        }
+        else{
+            NSLog("AEEEOW!")
+        }
+    }
+    
+    @IBAction func btn4(_ sender: Any) {
+        if AnswerNumber == 3{
+            PickQuestion(Question: 0)
+        }
+        else{
+            NSLog("AEEEOW!")
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
