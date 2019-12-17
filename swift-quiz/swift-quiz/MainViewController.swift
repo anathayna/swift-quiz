b3b505ce3adfd4eb3eef3169a71eaafd7902b50f
ANA THAYNA CONCEICAO FRANCA <ana.tcfranca@cas450193maca05.local>
2019-12-17 18:58:46 -0300

    adding launch storyboard

--- swift-quiz/swift-quiz/MainViewController.swift
+++ swift-quiz/swift-quiz/MainViewController.swift
@@ -13,43 +13,43 @@ class MainViewController: UIViewController {
     //MARK: Variables
     var tema1: [Question] = {
         
-        let question1 = Question(Question: "Quest˜indkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "AppleAcademy"))
-        let question2 = Question(Question: "Quest˜indkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
-        let question3 = Question(Question: "Quest˜indkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
-        let question4 = Question(Question: "Quest˜indkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "SenacLogo"))
+        let question1 = Question(Question: "Quesndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "AppleAcademy"))
+        let question2 = Question(Question: "Questndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        let question3 = Question(Question: "Qundkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        let question4 = Question(Question: "Questdkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "SenacLogo"))
         
         return [question1,question2,question3,question4]
         
     }()
     
-    var questions2: [Question] = {
+    var tema2: [Question] = {
         
-        let question1 = Question(Question: "Quest˜indkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "AppleAcademy"))
-        let question2 = Question(Question: "Quest˜indkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
-        let question3 = Question(Question: "Quest˜indkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "SenacLogo"))
-        let question4 = Question(Question: "Quest˜indkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
-        
-        return [question1,question2,question3,question4]
-        
-    }()
-    
-    var questions3: [Question] = {
-        
-        let question1 = Question(Question: "Quest˜indkjas ", Answers: [], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
-        let question2 = Question(Question: "Quest˜indkjas ", Answers: [], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
-        let question3 = Question(Question: "Quest˜indkjas ", Answers: [], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
-        let question4 = Question(Question: "Quest˜indkjas ", Answers: [], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        let question1 = Question(Question: "Questdkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "AppleAcademy"))
+        let question2 = Question(Question: "Questindkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        let question3 = Question(Question: "Quesndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "SenacLogo"))
+        let question4 = Question(Question: "Quesndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
         
         return [question1,question2,question3,question4]
         
     }()
     
-    var questions4: [Question] = {
+    var tema3: [Question] = {
         
-        let question1 = Question(Question: "Quest˜indkjas ", Answers: [], Answer: 2, Image: #imageLiteral(resourceName: "SenacLogo"))
-        let question2 = Question(Question: "Quest˜indkjas ", Answers: [], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
-        let question3 = Question(Question: "Quest˜indkjas ", Answers: [], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
-        let question4 = Question(Question: "Quest˜indkjas ", Answers: [], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        let question1 = Question(Question: "Questndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        let question2 = Question(Question: "Quesdkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        let question3 = Question(Question: "Quesndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        let question4 = Question(Question: "Quesndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        
+        return [question1,question2,question3,question4]
+        
+    }()
+    
+    var tema4: [Question] = {
+        
+        let question1 = Question(Question: "Quesndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "SenacLogo"))
+        let question2 = Question(Question: "Quesndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        let question3 = Question(Question: "Quesndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
+        let question4 = Question(Question: "Quesndkjas ", Answers: ["aaa","bbb","ccc","ddd"], Answer: 2, Image: #imageLiteral(resourceName: "LaunchImage"))
         
         return [question1,question2,question3,question4]
         
@@ -75,7 +75,7 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func onGoQuizTwo(_ sender: Any) {
-        let view = getQuizController(question: questions4)
+        let view = getQuizController(question: tema2)
         navigationController?.pushViewController(view, animated: true)
     }
     
